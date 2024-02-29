7a3b4522f22b8984463c2c8facdffbb9367df6e9
Roshan <1roshanekka@gmail.com>
2024-02-29 17:16:22 +0530

    Initial Commit

---
+++ Odyssey_of_Life/Odyssey_of_LifeApp.swift
@@ -0,0 +1,32 @@
+//
+//  Odyssey_of_LifeApp.swift
+//  Odyssey_of_Life
+//
+//  Created by Roshan Ekka on 29/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Odyssey_of_LifeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
